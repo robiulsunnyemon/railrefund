f771f09a6d3e73a6b4eb94a73de72c0cf42df03e
robiulsunnyemon <robiulsunyemon@gmail.com>
2026-09-14 15:04:11 +0600

    Update manifest to use new logo.png

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['pwa-192x192.svg', 'pwa-512x512.svg'],
+      includeAssets: ['logo.png'],
       devOptions: {
         enabled: true
       },
@@ -23,19 +23,14 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'pwa-192x192.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml'
+            src: 'logo.png',
+            sizes: '192x192 512x512',
+            type: 'image/png'
           },
           {
-            src: 'pwa-512x512.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml'
-          },
-          {
-            src: 'pwa-512x512.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml',
+            src: 'logo.png',
+            sizes: '192x192 512x512',
+            type: 'image/png',
             purpose: 'any maskable'
           }
         ]
